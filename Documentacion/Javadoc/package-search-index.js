5f2c04da3fd562d43cccdc91d8d475572dcfcf00
AlexRA95 <alexrodral95@gmail.com>
2025-02-04 17:29:41 +0100

    Se añade manual de usuario, javadoc del proyecto y de flujo de la aplicación
    Día 36, se añade el documento JavaDoc de todo el proyecto, también se añade el manual de usuario, junto a una tabla con usuarios de la aplicación ya creados para probar su funcionamiento

---
+++ Documentacion/Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"es.abatech.beans"},{"l":"es.abatech.controllers"},{"l":"es.abatech.DAO"},{"l":"es.abatech.DAOFactory"},{"l":"es.abatech.events"},{"l":"es.abatech.filters"},{"l":"es.abatech.models"}]
